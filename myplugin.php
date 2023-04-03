ea5528b8972003f663870cd7edcb11570d2033ab
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-04-03 16:24:04 +0530

    organized code into different files and folders

--- myplugin.php
+++ myplugin.php
@@ -7,45 +7,11 @@
  * Text Domain: elementor
  */
 
-function myplugin_tst_shortcode( $atts, $content='' ){
-    $atts = shortcode_atts( array(
-        'color' => '#0a0a0a',
-    ) , $atts); 
-    ob_start();
-    ?>
-    <div class="test">
-        <h2><?php echo $content; ?></h2>
-        <span style="color:<?php echo $atts['color'] ?>">testing</span>
-    </div>
-    <?php
-    return ob_get_clean();
-}
-add_shortcode('my-test-code' , 'myplugin_tst_shortcode');
+define( 'MYPLUGIN_FILE' , __FILE__ );
 
+require_once dirname(__FILE__) . '/includes/short_code.php';
+require_once dirname(__FILE__) . '/includes/custom_post_type.php';
 
-/* Custom Post Type Start */
-function create_posttype() {
-    // Register News post type
-    $args = array(
-        'public'=> true,
-        'label' => 'News',
-        'has_archieve' =>true,
-        'supports' => array('title', 'editor', 'excerpt', 'thumbnail')
-    );
-
-    register_post_type( 'news' , $args );
-    
-    // Register News Category taxonomy
-    register_taxonomy(
-        'news_category', 
-        'news',  
-        array(
-           'hierarchal' => true,
-           'label'=>'News Categories'
-        )
-    );
-}
-add_action( 'init', 'create_posttype' );
 
 function add_location_to_news( $content ){
     if( is_singular( 'news' ) )
@@ -54,9 +20,3 @@ function add_location_to_news( $content ){
 }
 add_filter( 'the_content' , 'add_loaction_to_news' );
 
-function posttype_activate(){
-    create_posttype();
-    flush_rewrite_rules();
-}
-
-register_activation_hook( __FILE__, 'posttype_activate' );
