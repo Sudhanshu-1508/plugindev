3c7a8b07c8ba7996e52688d9b84d5182ee404112
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-04-03 12:33:17 +0530

    shortcode

--- myplugin.php
+++ myplugin.php
@@ -1,6 +1,16 @@
+<?php
+
 /**
  * Plugin Name: myplugin
  * Author: Sudhanshu
  *
  * Text Domain: elementor
  */
+
+function myplugin_tst_shortcode( $atts ){
+    $atts = shortcode_atts( array(
+        'title'=> 'Default Title',
+    ) , $atts);
+    return '<div class="test"><h2>' . $atts['title'] . '</he>test</div>';
+}
+add_shortcode('my-test-shortcode' , 'myplugin_tst_shortcode');
