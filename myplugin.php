e8274ed9a537c0695fdfdd0b68b38a9a378e1e90
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-04-03 16:06:35 +0530

    cusotm post type with cusotm taxonomies

--- myplugin.php
+++ myplugin.php
@@ -21,3 +21,42 @@ function myplugin_tst_shortcode( $atts, $content='' ){
     return ob_get_clean();
 }
 add_shortcode('my-test-code' , 'myplugin_tst_shortcode');
+
+
+/* Custom Post Type Start */
+function create_posttype() {
+    // Register News post type
+    $args = array(
+        'public'=> true,
+        'label' => 'News',
+        'has_archieve' =>true,
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail')
+    );
+
+    register_post_type( 'news' , $args );
+    
+    // Register News Category taxonomy
+    register_taxonomy(
+        'news_category', 
+        'news',  
+        array(
+           'hierarchal' => true,
+           'label'=>'News Categories'
+        )
+    );
+}
+add_action( 'init', 'create_posttype' );
+
+function add_location_to_news( $content ){
+    if( is_singular( 'news' ) )
+    $content = ' <p class="news-loaction">Pune, India</p>' . $content;
+    return $content;
+}
+add_filter( 'the_content' , 'add_loaction_to_news' );
+
+function posttype_activate(){
+    create_posttype();
+    flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'posttype_activate' );
