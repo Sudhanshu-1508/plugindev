df6ed07a2aa437e563cee215e6668bd4ac6535e5
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-04-04 10:22:36 +0530

    added contents

--- myplugin.php
+++ myplugin.php
@@ -13,7 +13,8 @@ define('THEME_VERSION', '1.0');
 require_once dirname(__FILE__) . '/includes/short_code.php';
 require_once dirname(__FILE__) . '/includes/custom_post_type.php';
 require_once dirname(__FILE__) . '/includes/admin_settings.php';
-
+require_once dirname(__FILE__) . '/includes/news_content.php';
+require_once dirname(__FILE__) . '/includes/add_content.php';
 
 //function add_location_to_news( $content ){
 //    if( is_singular( 'news' ) )
@@ -22,26 +23,6 @@ require_once dirname(__FILE__) . '/includes/admin_settings.php';
 //}
 //add_filter( 'the_content' , 'add_loaction_to_news' );
 //
-function add_post_to_end_of_content( $content ){
-    global $post;
-    if( is_singular() ){
-        $args = array(
-            'numberposts' => 6
-        );
-        $latest_posts = get_posts( $args );
-        ob_start();
-        ?>
-        <ul class="latest-posts">
-            <?php foreach ( $latest_posts as $post) : ?>
-              
-                <?php setup_postdata( $post); ?>
-                <li><a href="<?php echo get_the_permalink( $post->ID);?>"><?php echo get_the_title( $post->ID); ?></li>
-                <?php endforeach; ?>
-            </ul>
-            <?php 
-            $content .= ob_get_clean();
-            wp_reset_postdata();
-    }
-    return $content;
-}
-add_filter( 'the_content', 'add_post_to_end_of_content');
+
+
+
